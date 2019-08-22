42bcfe1cda95f565e6653be68faa12813cf1298e
Patryk <piskorz.patryk.96@gmail.com>
2019-08-22 18:00:34 +0200

    Update TaskRestController.java

--- src/main/java/com/organizer/organizerapp/controller/rest/TaskRestController.java
+++ src/main/java/com/organizer/organizerapp/controller/rest/TaskRestController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import com.organizer.organizerapp.service.TaskService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskRestController {
 
 	@Autowired
