4c2e456638a97db97ea71f068d6bce92a221017c
Patryk <piskorz.patryk.96@gmail.com>
2019-09-18 16:51:38 +0200

    Create AuthenticationController.java

---
+++ src/main/java/com/organizer/organizerapp/controller/rest/AuthenticationController.java
@@ -0,0 +1,12 @@
+package com.organizer.organizerapp.controller.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class AuthenticationController {
+
+	
+
+}
