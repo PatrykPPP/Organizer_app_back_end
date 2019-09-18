974e4d618ea294fae031e04bbf328a6a548a6530
Patryk <piskorz.patryk.96@gmail.com>
2019-09-18 16:55:30 +0200

    Update AuthenticationController.java

--- src/main/java/com/organizer/organizerapp/controller/rest/AuthenticationController.java
+++ src/main/java/com/organizer/organizerapp/controller/rest/AuthenticationController.java
@@ -1,12 +1,15 @@
 package com.organizer.organizerapp.controller.rest;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 
-	
-
+	@GetMapping("/authenticaion")
+	public String getAuthentication() {
+		return "You are authenticated!";
+	}
 }
