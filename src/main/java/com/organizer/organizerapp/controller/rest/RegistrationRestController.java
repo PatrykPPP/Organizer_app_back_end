5e2fdadc8a8d88c934e72ff922b13f1ea5babe40
Patryk <piskorz.patryk.96@gmail.com>
2019-10-10 23:03:40 +0200

    update

--- src/main/java/com/organizer/organizerapp/controller/rest/RegistrationRestController.java
+++ src/main/java/com/organizer/organizerapp/controller/rest/RegistrationRestController.java
@@ -1,18 +1,27 @@
 package com.organizer.organizerapp.controller.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.organizer.organizerapp.repository.UserRepository;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class RegistrationRestController {
+	
+	@Autowired 
+	UserRepository userRepository;
 
 	@PostMapping("/registration")
 	public void register() {
 		
+		System.out.println(userRepository.findByUsername("Peter"));
+		
 	}
+	
 
 }
